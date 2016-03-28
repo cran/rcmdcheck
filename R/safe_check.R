bd792d5bd2aacda18bd7596612a96cd478167f37
Gabor Csardi <gcsardi@mango-solutions.com>
2016-03-28 14:05:29 +0000

    version 1.0.0

---
+++ R/safe_check.R
@@ -0,0 +1,13 @@
+
+safe_check_packages <- function(..., args, quiet) {
+
+  lib <- paste(.libPaths(), collapse = .Platform$path.sep)
+
+  with_envvar(
+    c(R_LIBS = lib,
+      R_LIBS_USER = lib,
+      R_LIBS_SITE = lib,
+      R_PROFILE_USER = tempfile()),
+    R("CMD", "check", ..., args, callback = if (!quiet) check_callback())
+  )
+}
